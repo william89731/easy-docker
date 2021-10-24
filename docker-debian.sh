6780ee43edc191ed35a73d1de9c2ebd74208e893
william <68069659+william89731@users.noreply.github.com>
2021-10-24 11:01:23 +0200

    Create docker-debian.sh

---
+++ docker-debian.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update system and install base packages
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl gnupg lsb-release
+
+# Add Docker’s official GPG key
+ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ 
+# Add the Docker repository 
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker & Docker Compose 
+sudo apt update
+sudo apt -y install docker-ce docker-ce-cli containerd.io
+
+# Add your user to run docker commands as non-privileged user
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Check docker version
+docker -v
+
+# Download docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
